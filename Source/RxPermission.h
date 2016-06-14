d8928e367cf16ca99a4adcd926b532f25e4e7d96
Andrew Breckenridge <asbreckenridge@me.com>
2016-06-14 11:20:51 -0700

    create xcodeproj

---
+++ Source/RxPermission.h
@@ -0,0 +1,19 @@
+//
+//  RxPermission.h
+//  RxPermission
+//
+//  Created by Andrew Breckenridge on 6/14/16.
+//  Copyright © 2016 Andrew Breckenridge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxPermission.
+FOUNDATION_EXPORT double RxPermissionVersionNumber;
+
+//! Project version string for RxPermission.
+FOUNDATION_EXPORT const unsigned char RxPermissionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxPermission/PublicHeader.h>
+
+
